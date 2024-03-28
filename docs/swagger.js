a9d02c72a3f6e480bbe014856625ce9d0007763d
Smart  Raysam <engsamtob@yahoo.com>
2024-03-28 10:52:47 +0100

    chore: Add swagger documentation

---
+++ docs/swagger.js
@@ -0,0 +1,65 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const options = {
+    openapi: 'OpenAPI 3',   // Enable/Disable OpenAPI. By default is null
+    language: 'en-US',      // Change response language. By default is 'en-US'
+    disableLogs: false,     // Enable/Disable logs. By default is false
+    autoHeaders: false,     // Enable/Disable automatic headers capture. By default is true
+    autoQuery: false,       // Enable/Disable automatic query capture. By default is true
+    autoBody: false         // Enable/Disable automatic body capture. By default is true
+}
+
+const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'Park Management System',
+    description: 'Park Management System API Documentation',
+  },
+  host: 'localhost:3009',
+  basePath: '/',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [
+    {
+      name: 'PMS',
+      description: 'Park Management System API',
+    },
+  ],
+  servers: [
+    {
+      url: "http://localhost:3009/",
+      description: "dev server"
+    }
+  ],
+  securityDefinitions: {
+    apiKeyAuth: {
+      type: 'apiKey',
+      in: 'header', // can be "header", "query" or "cookie"
+      name: 'X-API-KEY', // name of the header, query parameter or cookie
+      description: 'any description...',
+    },
+  },
+  definitions: {
+    User: {
+      name: 'Adeseluka Toba Samuel',
+      diplomas: [
+        {
+          school: 'ALX University',
+          year: 2024,
+          completed: true,
+        },
+      ],
+    },
+    AddUser: {
+      $name: 'Anthony Ifeanyi Nwanbosi',
+      about: '',
+    },
+  },
+};
+
+const outputFile = './docs/swagger-output.json';
+const endpointsFiles = ['./routes/index.js'];
+swaggerAutogen(options)(outputFile, endpointsFiles, doc).then(async () => {
+  await import('./server.js');
+});
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import router from './routes';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3009;
 
 app.use(express.json());
 app.use(router);
@@ -11,5 +11,4 @@ app.use(router);
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
-
 export default app;
